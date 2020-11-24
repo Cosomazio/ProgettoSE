a5b8a0ff542c7bf344df2a48dbd2f122a9e167a9
franc <franc@LAPTOP-2RV6G94A>
2020-11-24 18:26:56 +0100

    è una prova nuova

---
+++ projectwork/src/prov1/micino.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prov1;
+
+/**
+ *
+ * @author franc
+ */
+public class micino {
+    public void miagola(){
+        for (int i=0; i<3; i++){
+            System.out.println("miao");
+        }
+    }
+    
+}
